3719c9bf2a1fff0c72b8b43063dab17e446dbb67
Cody Toney-Griffith <cody.toney@gmail.com>
2019-12-22 06:43:05 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def badge_maker(na
+  badge_maker = ["Arel"]
+  badge_maker.each do |name|
+  puts "Hello, my name is #{name}."
+end 
+end
+
+def batch_badge_creator(attendees)
+  attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  attendees.each do |badges|
+    puts "Hello my name is #{badges}."
+  end
+end 
